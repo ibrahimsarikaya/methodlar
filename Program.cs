77af52e3d88ceeb3c16bccf769fe915979ea4ddd
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-01 20:40:08 +0300

    Codding

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace methodlar // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
